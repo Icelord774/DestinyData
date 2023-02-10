832426c4f082d12553f3b79062c9762e953360e2
Shravan Sunil Menon <S.SUNIL-MENON-2020@hull.ac.uk>
2023-02-10 13:17:39 +0000

    testing code

--- main.py
+++ main.py
@@ -1,25 +1,30 @@
 import requests
 
-# Your API key
-api_key = "207ffa2b218540719501f16ea1551620"
+activity_id = "12328300571"
 
 # The endpoint URL
-endpoint = "/Platform/Destiny2/Stats/Definition/"
+endpoint = f"https://www.bungie.net/Platform/Destiny2/Stats/PostGameCarnageReport/{activity_id}/"
 
 # Add your API key to the headers
 headers = {
-    "X-API-Key": api_key
+    "X-API-Key": "207ffa2b218540719501f16ea1551620"
 }
 
 # Make the API request
-response = requests.get(endpoint, headers=headers)
+response = requests.get(endpoint, headers=headers).json()
 
 # Check the status code of the response
-if response.status_code == 200:
-    # Parse the JSON data
-    data = response.json()
-    # Do something with the data
-    print(data)
-else:
-    # Handle the error
-    print("An error occurred:", response.text)
+
+# Do something with the data
+teams = response["Response"]["entries"]
+
+for team in teams:
+
+    score = team["score"]
+
+    print("Score:", score)
+    print("Players:")
+
+    # END FOR
+
+
